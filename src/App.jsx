e70c437edde9c0e2f13fa57d83ad4a965baeced5
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2026-09-23 17:57:17 +0300

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-
 import { Component } from 'react'
 import './App.css'
 
-
 import tasks from "./tasks.json"
 
-class App extends Component{
+class App extends Component {
 
   state = {
     tasks,
@@ -17,20 +15,20 @@ class App extends Component{
     }))
   }
 
-
-  render(){
-    return(
-      <ul>{this.state.tasks.map((tasks) => {
-        return (
-          <li key={tasks.id}>
-            <p>{tasks.text}</p>
-            <button onClick={() => this.handleTasksDelete(tasks.id)} type="submit">видалити</button>
-          </li>
-        )
-      })}</ul>
+  render() {
+    return (
+      <ul>
+        {this.state.tasks.map((tasks) => {
+          return (
+            <li key={tasks.id}>
+              <p>{tasks.text}</p>
+              <button onClick={() => this.handleTasksDelete(tasks.id)} type="button">видалити</button>
+            </li>
+          )
+        })}
+      </ul>
     )
   }
 }
-
 
 export default App
