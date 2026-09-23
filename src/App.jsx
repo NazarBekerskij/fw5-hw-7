c74b2e74091c9498708008263009d2203fe18874
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2026-09-23 16:51:04 +0300

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,36 @@
+
+import { Component } from 'react'
 import './App.css'
 
 
+import tasks from "./tasks.json"
+
+class App extends Component{
+
+  state = {
+    tasks,
+  }
+  
+  handleTasksDelete = (id) => {
+    this.setState((prev) => ({
+      tasks: prev.tasks.filter(tasks => tasks.id !== id)
+    }))
+  }
+
+
+  render(){
+    return(
+      <ul>{this.state.tasks.map((tasks) => {
+        return (
+          <li key={tasks.id}>
+            <p>{tasks.text}</p>
+            <button onClick={() => this.handleTasksDelete(tasks.id)} type="submit">видалити</button>
+          </li>
+        )
+      })}</ul>
+    )
+  }
+}
+
+
+export default App
